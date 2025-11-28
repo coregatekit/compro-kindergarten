66b979dd84a5ae9759e49caae32efe56dae594c3
Joe Kim <joesm.genie@gmail.com>
2025-11-28 16:33:21 +0700

    Add example console.log

---
+++ src/examples/hello_world.ts
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+console.log("How old are you?");
+console.log("This is a simple TypeScript program.");
+
+console.log(12345);
+console.log(123.45);
+
+console.log(true);
+console.log(false);
